828e48fc6b108ce6b276b1ba28599767f1e7ccdd
aminov <alami@tshtt.uz>
2022-02-15 17:55:49 +0500

    ch3-002.0 Category-Create-Validation on Server-side

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,9 +24,13 @@ namespace asp_net_core_mvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -6,10 +6,12 @@ namespace asp_net_core_mvc.Models
     public class Category
     {
         [Key]
-        public int Id { get; set; }
-        [DisplayName("Name")]
+        public int Id { get; set; }        
+        [Required]
         public string Name { get; set; }
-        [DisplayName("Display Order")] 
+        [DisplayName("Display Order")]
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage ="Display Order fot category must be greater than 0")]
         public int DisplayOrder { get; set; }
     }
 }

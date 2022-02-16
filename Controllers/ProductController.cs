78c0420993a03d6929344f2fba457b03a3b8020e
aminov <alami@tshtt.uz>
2022-02-16 20:35:15 +0500

    ch4-006 Product M-V-C v.1 Upsert

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using asp_net_core_mvc.Data;
+using asp_net_core_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_core_mvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+            foreach (Product obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(u => u.Id == obj.CategoryId);
+            }
+            return View(objList);
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new Product();
+            if (id == null)
+            {
+                return View(product); //new -> insert
+            } else
+            {
+                product = _db.Product.Find(); //update
+                if(product==null) { return NotFound(); }
+            }
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+             if (ModelState.IsValid)
+            {
+                _db.Product.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+            Product obj = _db.Product.Find(id);
+            if (obj == null) { return NotFound(); }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+                var obj = _db.Product.Find(id);
+                if (obj == null) { return NotFound(); }
+                _db.Product.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            return View(obj);
+        }
+
+
+    }
+}

5921f402c93d3d6e9f21cb234e01e8aba71cdc00
aminov <alami@tshtt.uz>
2022-02-21 15:05:38 +0500

    ch6-003 Model/App..User; migration FullName column in AspNetUsers

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace asp_net_core_mvc.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}

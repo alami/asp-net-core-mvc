89022c39feb788e4ee6f669c49734aa3903e72d9
aminov <alami@tshtt.uz>
2022-02-22 20:03:07 +0500

    ch7-006 ShoppingCart with Authirize, Summery

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,13 +1,19 @@
 ﻿using asp_net_core_mvc.Data;
 using asp_net_core_mvc.Models;
+using asp_net_core_mvc.Models.ViewModels;
 using asp_net_core_mvc.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace asp_net_core_mvc.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
         public CartController(ApplicationDbContext db)
         {
             _db = db;
@@ -15,8 +21,8 @@ namespace asp_net_core_mvc.Controllers
         public IActionResult Index()
         {
             List<ShoppingCart> shoppingCartsList = new List<ShoppingCart>();
-            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
-                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0)
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
             {
                 shoppingCartsList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
             }
@@ -39,5 +45,40 @@ namespace asp_net_core_mvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+
+            return RedirectToAction(nameof(Summary));
+        }
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exsits
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = prodList.ToList()
+            };
+
+
+            return View(ProductUserVM);
+        }
     }
+    
 }
---
+++ Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,13 @@
+﻿namespace asp_net_core_mvc.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public IList<Product> ProductList { get; set; }
+    }
+}

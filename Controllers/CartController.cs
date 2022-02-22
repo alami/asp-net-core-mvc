59741625134f5945c2055e59e9ae483f723c1d1b
aminov <alami@tshtt.uz>
2022-02-22 18:48:49 +0500

    ch7-003 ShoppingCart-Index,Remove

---
+++ Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using asp_net_core_mvc.Data;
+using asp_net_core_mvc.Models;
+using asp_net_core_mvc.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_core_mvc.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCartsList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0)
+            {
+                shoppingCartsList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            List<int> prodInCart = shoppingCartsList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+            return View(prodList);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartsList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartsList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartsList.Remove(shoppingCartsList.FirstOrDefault(u => u.ProductId == id));
+
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartsList);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
